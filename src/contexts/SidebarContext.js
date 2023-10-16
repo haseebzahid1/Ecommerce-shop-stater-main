3b61ad85b5d945287b5f647e753b930834f327c5
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-16 20:07:26 +0500

    Build SidebarProvider  Component

--- src/contexts/SidebarContext.js
+++ src/contexts/SidebarContext.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, createContext } from "react";
 
-const SidebarContext = () => {
-  return <div>SidebarContext</div>;
+export const SidebarContext = createContext();
+const SidebarProvider = ({ children }) => {
+  // sidebar state
+  const [isOpen, setIsopen] = useState(false);
+
+  const handleClose = () => {
+    setIsopen(false)
+  };
+
+  return (
+    <SidebarContext.Provider 
+      value={{isOpen, setIsopen, handleClose}} >
+       {children}
+    </SidebarContext.Provider>
+  )
 };
 
-export default SidebarContext;
+export default SidebarProvider;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 //product provider
 import ProductProvider from "./contexts/ProductContext";
+import SidebarProvider from "./contexts/SidebarContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductProvider>
-    <React.StrictMode>
-      <BrowserRouter>
-        {" "}
-        <App />{" "}
-      </BrowserRouter>
-    </React.StrictMode>
-  </ProductProvider>
+  <SidebarProvider>
+    <ProductProvider>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </ProductProvider>
+  </SidebarProvider>
 );
