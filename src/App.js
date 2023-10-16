b0051b3911975acfba2ae9c8346612890649a5dd
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-16 19:38:46 +0500

    Build product card with fake api data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Home from './pages/Home'
-import ProductDetails from './pages/ProductDetails'
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Footer from './components/Footer';
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <React.Fragment>
-    
-  <Header />
+      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
-     <Sidebar />
-     <Footer />
+      <Sidebar />
+      <Footer />
     </React.Fragment>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,26 @@
 import React, { useContext } from "react";
+// impoart product context
 import { ProductContext } from "../contexts/ProductContext";
+// import components
+import Product from '../components/Product'
 
 const Home = () => {
   // get products from product context
   const { products } = useContext(ProductContext);
-
+// get only men's & women's clothing category
   const filteredProducts = products.filter((item) => {
     return (
       item.category === "men's clothing" || item.category === "women's clothing"
     );
   });
-  // console.log(filteredProducts)
+  // console.log(filteredProducts)g
 
   return <section className="py-16">
     <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-colos-4 xl:grid-cols-5 gap-[30px]
       max-w-sm mx-auto md:max-w-none md:mx-0">
         {filteredProducts.map(product => {
-         return (
-          <div className="w-full h-[300px] bg-pink-200" key={product.id}>
-            {product.title}
-          </div>
-         )
-          
+         return <Product product={product} key={product.id} />
         })}
       </div>
     </div>
