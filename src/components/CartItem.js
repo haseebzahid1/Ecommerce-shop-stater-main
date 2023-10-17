b8e9fcc2a824304bf9af7097719abb6c7d2e957e
haseebzahid1 <mmianhaseeb444@gmail.com>
2023-10-17 20:29:02 +0500

    remove button  added in cartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React,{ useContext } from "react";
 import { Link } from "react-router-dom";
 import { IoMdClose, IoMdAdd, IoMdRemove } from "react-icons/io";
+import { CartContext } from "../contexts/CartContext";
+
 const CartItem = ({ item }) => {
+  const { removeFromCart } = useContext(CartContext);
+  // destructure item
   const { id, title, image, price, amount } = item;
   return (
     <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
@@ -21,7 +25,7 @@ const CartItem = ({ item }) => {
               {title}
             </Link>
             {/* remove icon */}
-            <div className="text-xl cursor-pointer">
+            <div onClick={() => removeFromCart(id)} className="text-xl cursor-pointer">
               <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
             </div>
           </div>
